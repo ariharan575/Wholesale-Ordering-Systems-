fd6a4a739f896a78f540e5d52dcc29f1e839310e
ARIHARAN <fmoon6795@gmail.com>
2026-04-27 13:23:24 +0530

    First Commit

---
+++ Backend/src/main/java/com/backend/MarketLinker/MarketLinkerApplication.java
@@ -0,0 +1,13 @@
+package com.backend.MarketLinker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketLinkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketLinkerApplication.class, args);
+	}
+
+}
